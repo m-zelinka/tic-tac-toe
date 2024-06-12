e8d4fd5f91eec898455a76ca03533ac3f5be729d
Marek Zelinka <mzelinka17@gmail.com>
2024-06-12 17:58:20 +0200

    make component interactive

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,13 @@
 import clsx from "clsx";
-import PropTypes from "prop-types";
+import { useState } from "react";
 
-function Square({ value }) {
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+
   return (
     <button
       type="button"
@@ -13,13 +19,11 @@ function Square({ value }) {
         "bg-amber-400",
         "bg-white ring-1 ring-inset ring-gray-300 hover:enabled:bg-gray-50 dark:bg-white/10 dark:ring-0 dark:hover:enabled:bg-white/20",
       )}
+      onClick={handleClick}
     >
       {value}
     </button>
   );
 }
-Square.propTypes = {
-  value: PropTypes.string.isRequired,
-};
 
 export default Square;
