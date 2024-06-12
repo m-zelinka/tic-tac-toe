0f055e1a831f7beb734fc6bce82f64723967d6dd
Marek Zelinka <mzelinka17@gmail.com>
2024-06-12 17:54:19 +0200

    build the game board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,6 +4,14 @@ function Board() {
   return (
     <div className="grid grid-cols-3 gap-5">
       <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
     </div>
   );
 }
