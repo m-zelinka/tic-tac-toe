ad93e1d27632139ed502a4f0857686688d43f5e2
Marek Zelinka <mzelinka17@gmail.com>
2024-06-24 12:11:05 +0200

    fix player icons clip bug

--- src/components/OIcon.jsx
+++ src/components/OIcon.jsx
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 
 function OIcon({ className }) {
   return (
-    <svg viewBox="0 0 64 64" className={className} aria-hidden>
-      <path d="M32 0a32 32 0 1 1 0 64 32 32 0 0 1 0-64Zm0 18.96a13.04 13.04 0 1 0 0 26.08 13.04 13.04 0 0 0 0-26.08Z" />
+    <svg viewBox="0 0 66 66" className={className} aria-hidden>
+      <path d="M33 1a32 32 0 1 1 0 64 32 32 0 0 1 0-64Zm0 18.96a13.04 13.04 0 1 0 0 26.08 13.04 13.04 0 0 0 0-26.08Z" />
     </svg>
   );
 }
--- src/components/XIcon.jsx
+++ src/components/XIcon.jsx
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 
 function XIcon({ className }) {
   return (
-    <svg viewBox="0 0 64 64" className={className} aria-hidden>
-      <path d="m15 1.15 17 17 17-17a3 3 0 0 1 4.24 0l9.61 9.6a3 3 0 0 1 0 4.25l-17 17 17 17a3 3 0 0 1 0 4.24l-9.6 9.61a3 3 0 0 1-4.25 0l-17-17-17 17a3 3 0 0 1-4.24 0l-9.61-9.61a3 3 0 0 1 0-4.24l17-17-17-17a3 3 0 0 1 0-4.24l9.6-9.61a3 3 0 0 1 4.25 0Z" />
+    <svg viewBox="0 0 66 66" className={className} aria-hidden>
+      <path d="m16 2.15 17 17 17-17a3 3 0 0 1 4.24 0l9.61 9.6a3 3 0 0 1 0 4.25l-17 17 17 17a3 3 0 0 1 0 4.24l-9.6 9.61a3 3 0 0 1-4.25 0l-17-17-17 17a3 3 0 0 1-4.24 0l-9.61-9.61a3 3 0 0 1 0-4.24l17-17-17-17a3 3 0 0 1 0-4.24l9.6-9.61a3 3 0 0 1 4.25 0Z" />
     </svg>
   );
 }
